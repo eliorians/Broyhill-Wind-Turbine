04bba664a072c8ac2310ce3e3686d94e96e3c10e
Eli Orians <eli.orians@gmail.com>
2023-09-27 15:23:21 -0400

    wind data cleaned

--- data-util.py
+++ data-util.py
@@ -3,20 +3,22 @@ import pandas as pd
 def main():
 
     #data into dataframes
-    windData = pd.read_csv('./data/wind-data.csv')
-    weatherData = pd.read_csv('./data/weather-data.csv')
+    windData = pd.read_csv('./data/raw/wind-data.csv')
+    weatherData = pd.read_csv('./data/raw/weather-data.csv')
 
     #data cleaning
     windData = cleanWind(windData)
 
     print(windData)
 
-
 def cleanWind(windData):
     #sort by date/time
     windData = windData.sort_values(by=['datadatetime']) 
     #drop duplicates
     windData = windData.drop_duplicates()
+    #convert datadatetime to a timestamp and aggregate to hourly by taking the average production
+    windData['datadatetime'] = pd.to_datetime(windData['datadatetime'])
+    windData = windData.groupby(pd.Grouper(key='datadatetime', freq='H')).mean()
 
     return windData
 
