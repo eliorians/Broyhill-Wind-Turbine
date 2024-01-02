5d38ba4fc6a1ddf401fc2e8ab805de6556087454
eli <eli.orians@gmail.com>
2024-01-02 12:57:12 -0500

    cleaned up forecast-util

--- forecast-util.py
+++ forecast-util.py
@@ -23,17 +23,25 @@ def logging_setup():
             logging.FileHandler(log_file)  # Save log messages to a file in the "logs" directory
         ])
 
-def convert_filepath(filepath):
-    # extract the directory path and filename from the original filepath
-    directory, filename = os.path.split(filepath)
-    # remove the seconds, and change the extension to .csv
-    base_name = re.sub(r'(_|\d+)\.json$', '.csv', filename)
-    base_name = base_name.rsplit('-', 1)[0] + '.csv'
-    # Construct the new directory path with "-processed" and the new filename
-    new_directory = os.path.join(os.path.dirname(directory), 'forecast-data-processed')
-    new_filepath = os.path.join(new_directory, base_name)
-    return new_filepath
+#converts all forecast json files to csv files
+def jsonToCSV():
+    start_time = time.time()
+    file_count = 0
 
+    #iterate through all files in the forecast-data folder
+    for filename in os.listdir(json_folder):
+        if filename.endswith('.json'):
+            # Construct the full path to the JSON file
+            filepath = os.path.join(json_folder, filename)
+            # Process the JSON file
+            process_json_file(filepath)
+            file_count = file_count + 1
+
+    end_time = time.time()
+    runtime = end_time - start_time
+    logger.info(f"Program finished. Processed {file_count} in {runtime:.2f} seconds")
+
+#process individual json file
 def process_json_file(filepath):
 
     #open json file
@@ -45,31 +53,31 @@ def process_json_file(filepath):
     df = pd.DataFrame(periods)
 
     #save to csv
-    filepath = convert_filepath(filepath)
+    filepath = process_filepath(filepath)
     df.to_csv(filepath)
     logger.info(f"Data successfully saved to {filepath}")
 
-              
+#converts forecast path to csv path
+def process_filepath(filepath):
+    # extract the directory path and filename from the original filepath
+    directory, filename = os.path.split(filepath)
+    # remove the seconds, and change the extension to .csv
+    base_name = re.sub(r'(_|\d+)\.json$', '.csv', filename)
+    base_name = base_name.rsplit('-', 1)[0] + '.csv'
+    # Construct the new directory path with "-processed" and the new filename
+    new_directory = os.path.join(os.path.dirname(directory), 'forecast-data-processed')
+    new_filepath = os.path.join(new_directory, base_name)
+    return new_filepath
+
 def main():
 
-    start_time = time.time()
-    file_count = 0
     logging_setup()
     logger.info("Running forecast-util...")
+    
+    #convert all forecast files to json
+    jsonToCSV()
 
-    #iterate through all files in the forecast-data folder
-    for filename in os.listdir(json_folder):
-        if filename.endswith('.json'):
-            # Construct the full path to the JSON file
-            filepath = os.path.join(json_folder, filename)
-            # Process the JSON file
-            process_json_file(filepath)
-            file_count = file_count + 1
-
-
-    end_time = time.time()
-    runtime = end_time - start_time
-    logger.info(f"Program finished. Processed {file_count} in {runtime:.2f} seconds")
+    
 
 if __name__ == '__main__':
     main()
